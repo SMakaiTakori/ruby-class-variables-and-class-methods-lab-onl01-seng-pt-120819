90351b7cf33d086eecffc9e625d03f73a02c7bea
S. Makai Takori <makait87@gmail.com>
2020-01-09 16:52:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,14 +21,12 @@ class Song
     
   def self.genre_count
     @@genres.each do |genre|
-      
-      
       if genre_count[genre]
       @@grenres += 1 
       else
         genre_count[genre] = 1
-        
-        
+      end
+    end  
   end  
     
     
