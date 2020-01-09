994763804e33c9324f75ac77384fb1ded10f3e25
S. Makai Takori <makait87@gmail.com>
2020-01-09 16:21:53 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song
+  
+  @@count = 0
+  @@genres = []
+  @@artists= []
+  
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name,artist,genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
+
+  def self.count
+    @@count 
+  end  
+    
+  def self.genre_count
+    
+  end  
+    
+    
+    
+    
+end  
